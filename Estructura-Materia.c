ad9176086c93f82c1002d96b3c854573cb97c597
Ddani28 <dantoniochp@gmail.com>
2025-04-25 18:54:11 -0600

    Estructura para las materias

--- Estructura-Materia.c
+++ Estructura-Materia.c
@@ -3,57 +3,135 @@
 struct Materias 
 {
 	int clave, semestre;
-	char nombre[20], descripcion[50];	
+	char nombre[20], descripcion[20];	
 };
 
 
 main()
 {
-	struct Materias materia;
+	struct Materias mt[50];
+	int totalMaterias = 0, opcion;
+	char op[2];
+	
+	do
+	{
+        printf("\nMenu principal \n");
+        printf("1. Alumnos\n");
+        printf("2. Profesores\n");
+        printf("3. Materias\n");
+        printf("4. Grupo\n");
+        printf("5. Inscripcion\n");
+        printf("6. Actualizar promedio\n");
+        printf("7. Reportes\n");
+        printf("8. Salir\n");
+        printf("Opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+		{
+            case 1:
+                
+                break;
+           
+            case 2:
+               
+                break;
+                
+            case 3:
+            	do
+				{
+            		ingresarMaterias(&mt[totalMaterias++]);
+            		printf("Desea agregar otra materia: ");
+            		fgets(op, 2, stdin);
+            		if (op == 'No')
+            			printf("Regresando al menu principal");	
+            	}while(op == 'Si');
+            	
+                break;
+                
+            case 4:
+               
+                break;
+                
+            case 5:
+               
+                break;
+        	case 6:
+               
+                break;
+            case 7:
+               
+                break;
+            
+			case 8:
+               
+                break;
+            
+            default:
+                printf("Opción no valida.\n");
+        }
+    } while (opcion != 0);
 }
 
-int validarClaveMaterias(struct Materias materia)
-{	
-	printf("Ingrese la clave de la materia: ");
-	scanf("%d", &materia.clave);
-	if (materia.clave > 0)
-	{
-		printf("Agregado correctamente");
-		return materia.clave;
-	}
-	else
-	{
-		printf("Ingrese una clave mayor a cero");
-		scanf("%d", &materia.clave);
-	}
-}
-
-int validarSemestreMaterias(struct Materias materia)
+void ingresarMaterias(struct Materias mt[])
 {
-	printf("Ingrese el semestre de la materia: ");
-	scanf("%d", &materia.semestre);
-	if (materia.clave > 0 && materia.clave <= 10)
+	int i, clave, semestre;
+	char nombre[20], descripcion[30], nombreSinValidar[20], descSinValidar[30];
+	/*for (i = 0; i < 2; i++)
 	{
-		printf("Agregado correctamente");
-		return materia.semestre;
-	}
-	else
-	{
-		printf("Ingrese un semestre entre cero y diez");
-		scanf("%d", &materia.semestre);
-	}
+		if (mt[i].clave != '\n' && mt[i].semestre != '\n' 
+		&& mt[i].nombre != '\n' && mt[i].descripcion != '\n')  //mt[i).clave mt[i).semestre etc
+		{*/
+			do {
+				printf("\nIngresa la clave de la materia: ");
+				scanf("%d", &clave);
+				if (clave < 0)
+					printf("Ingrese una clave mayor a 0");
+			}
+			while(clave < 0);
+			
+			mt[i].clave = clave;
+				
+			do {
+				printf("\nIngresa el semestre de la materia: ");
+				scanf("%d", &semestre);
+				if (semestre < 0 || semestre > 10)
+					printf("Ingrese un semestre mayor a 0 y menor a 10");
+			}
+			while(semestre < 0 || semestre > 10);
+			
+			mt[i].semestre = semestre;
+				
+			do {
+				printf("\nIngresa el nombre de la materia: ");
+				fflush(stdin);
+				scanf("%s", &nombreSinValidar);
+				//fgets(nombre, 20, stdin);
+			if (strlen(nombreSinValidar) == 0) 
+			{
+        		printf("El nombre no puede estar vacío. Intente de nuevo.\n");
+    		}
+			}while (strlen(nombreSinValidar) == 0);
+			
+			strcpy(mt[i].nombre, nombreSinValidar);
+			
+			do {
+				printf("\nIngresa la descripcion de la materia: ");
+				fflush(stdin);
+				scanf("%s", &descSinValidar);
+				//fgets(descripcion, 30, stdin);
+			if (strlen(descSinValidar) == 0) 
+			{
+        		printf("El nombre no puede estar vacío. Intente de nuevo.\n");
+    		}
+			}while (strlen(descSinValidar) == 0);
+			
+			strcpy(mt[i].descripcion, descSinValidar);
+	/*	}
+			printf("Cupo de Materias lleno");
+	}*/
+	
 }
-
-void validarNombre(struct Materias materia)
-{
-	//No este vacia la cadena
-}
-
-void validarDescripcionMateria(struct Materias materia)
-{
-	//No este vacia la cadena
-}
-
 
 
 
