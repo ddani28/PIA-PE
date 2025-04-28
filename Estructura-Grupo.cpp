a026eee0e5509ca9df8ae92d34d766784392139e
Dany-tech-maker <danotorrepan@gmail.com>
2025-04-27 22:48:09 -0600

    Update Estructura-Grupo.cpp

--- Estructura-Grupo.cpp
+++ Estructura-Grupo.cpp
@@ -15,7 +15,7 @@ main()
 
 void ingresarGrupos(struct Grupos gp[])
 {
-	int i, clave, semestre, claveMateria, numEmpleado;
+	int i, clave, semestre, claveMateria, numEmpleado, dia, mes, anio;
 	
 	do 
 	{
@@ -33,6 +33,7 @@ void ingresarGrupos(struct Grupos gp[])
 		scanf("%d", &semestre);
 		if (semestre < 0 || semestre > 10)
 			printf("Ingrese un semestre mayor a 0 y menor a 10");
+		
 	}while(semestre < 0 || semestre > 10);
 			
 	gp[i].semestre = semestre;
@@ -44,9 +45,8 @@ void ingresarGrupos(struct Grupos gp[])
 		scanf("%s", &claveMateria);
 				
 		if (strlen(claveMateria) == 0) 
-		{
-        	printf("La clave no puede estar vac�o. Intente de nuevo.\n");
-    	}
+        	printf("La clave no puede estar vacío. Intente de nuevo.\n");
+		
 	}while (strlen(claveMateria) == 0);
 			
 	strcpy(gp[i].claveMateria, claveMateria);
@@ -58,13 +58,32 @@ void ingresarGrupos(struct Grupos gp[])
 		fflush(stdin);
 		scanf("%s", &numEmpleado);
 		if (strlen(numEmpleado) == 0) 
-		{
         	printf("El numero de empleado no puede estar vacio. Intente de nuevo.\n");
-    	}
+		
 	}while (strlen(numEmpleado) == 0);
 			
 	strcpy(gp[i].numEmpleado, numEmpleado);
 	
+	
+	time_t t = time(NULL);
+	struct tm fechaActual = *localtime(&t);
+
+	do
+	{
+		printf("Ingrese su fecha de grupo:\n");
+		printf("Dia: ");
+		scanf("%d", &dia);
+		printf("Mes: ");
+		scanf("%d", &mes);
+		printf("Año: ");
+		scanf("%d", &anio);
+		
+		if( dia > fechaActual.tm_mday || mes > fechaActual.tm_mon + 1 || anio > fechaActual.tm_year + 1900)
+			printf("La fecha ingresada es mayor que la fecha actual. Intente nuevamente.\n");
+		
+	}while (dia < fechaActual.tm_mday && mes < fechaActual.tm_mon + 1 && anio < fechaActual.tm_year + 1900)
+		
+    sprintf(gp[i].fecha, "%02d/%02d/%04d", dia, mes, anio);
 	/*Falta fecha y periodo asi como validar claveMateria y numEmpleado con sus estructuras
 	respectivas*/
 		
